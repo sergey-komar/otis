7d2319ee6078570f1a53917e9878c57eb6b4e767
sergey-komar <kursev81@yandex.ru>
2025-07-30 12:59:45 +0300

    word mobile

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,21 @@ $(function () {
     $(".safety-slider").slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+         responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+               slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 800,
+              settings: {
+               slidesToShow: 1,
+              }
+            },
+           
+          ]
        
       });
 
@@ -42,6 +57,27 @@ $(function () {
     $(".updates-slider").slick({
         slidesToShow: 4,
         slidesToScroll: 1,
+         responsive: [
+            {
+              breakpoint: 1250,
+              settings: {
+               slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 950,
+              settings: {
+               slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 650,
+              settings: {
+               slidesToShow: 1,
+              }
+            },
+           
+          ]
        
       });
 
@@ -49,6 +85,21 @@ $(function () {
     $(".story-slider").slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+         responsive: [
+            {
+              breakpoint: 1250,
+              settings: {
+               slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 850,
+              settings: {
+               slidesToShow: 1,
+              }
+            },
+           
+          ]
        
       });
 
@@ -69,4 +120,36 @@ window.addEventListener('DOMContentLoaded', () => {
       menu.classList.toggle("nav--active");
       document.body.classList.toggle('overflow')
     });
+
+
+     const btn = document.querySelector('.btnUp');
+      const headerWrapper = document.querySelector('.header');
+
+        if(btn){
+           btn.addEventListener('click', ()=>{
+            window.scrollTo({
+            top: 0, 
+            behavior: 'smooth' 
+            });
+          });
+        }
+       
+        function up() {
+
+
+            
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 690){
+            btn.classList.add('btnUp-visible');
+            headerWrapper.classList.add('box-shadow');
+            }else{
+            btn.classList.remove('btnUp-visible');
+            headerWrapper.classList.remove('box-shadow');
+            }
+        })
+        }
+        up();
+
+
+
 })
